a075f309c46f35ea9745caad32b4a497212b0eab
George Martsoukos <netdark@hotmail.gr>
2018-11-25 22:51:20 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,35 +3,35 @@ const options = document.querySelectorAll(".myselect option");
 
 // 1st method
 select.addEventListener("change", function() {
-	const url = this.options[this.selectedIndex].dataset.url;
-	if(url) {
-		location.href = url;
-	}
+  const url = this.options[this.selectedIndex].dataset.url;
+  if(url) {
+    location.href = url;
+  }
 });
 
 for(const option of options) {
-	const url = option.dataset.url;
-	if(location.href.includes(url)) {
+  const url = option.dataset.url;
+  if(location.href.includes(url)) {
     option.setAttribute("selected", true);
-		break;
+    break;
   }
 }
 
 // 2nd method
 /*select.addEventListener("change", function() {
-	const url = this.options[this.selectedIndex].dataset.url;
-	if(url) {
-		localStorage.setItem("url", url);
-		location.href = url;
-	}
+  const url = this.options[this.selectedIndex].dataset.url;
+  if(url) {
+    localStorage.setItem("url", url);
+    location.href = url;
+  }
 });
 
 if(localStorage.getItem("url")) {
-	for(const option of options) {
-		const url = option.dataset.url;
-		if(url === localStorage.getItem("url")) {
-	    option.setAttribute("selected", true);
-	    break;
-	  }
-	}
+  for(const option of options) {
+    const url = option.dataset.url;
+    if(url === localStorage.getItem("url")) {
+      option.setAttribute("selected", true);
+      break;
+    }
+  }
 }*/
